455cf7ef55bd3a764add441e07d37899bf07d696
sidrusiya <51047433+sidrusiya@users.noreply.github.com>
2021-01-28 00:25:52 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -9,11 +9,11 @@ function Footer() {
                 <h5>All rights reserved | 2021</h5>
             </div>
             <div className="fb-end">
-                <a href="https://www.linkedin.com/in/siddhartha-rusiya-79478a185/" target="_blank">
-                <img width="50" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png"/>
+                <a href="https://www.linkedin.com/in/siddhartha-rusiya-79478a185/" target="_blank" rel="noreferrer">
+                <img width="50" alt="code" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png"/>
                 </a>
-                <a href="https://github.com/sidrusiya" target="_blank">
-                <img width="50" src="https://img.favpng.com/23/3/8/github-computer-icons-clip-art-png-favpng-CvXXq7gfsGZZJdnv1RK6GAXNk_t.jpg"/>
+                <a href="https://github.com/sidrusiya" target="_blank" rel="noreferrer">
+                <img width="50" alt="favour" src="https://img.favpng.com/23/3/8/github-computer-icons-clip-art-png-favpng-CvXXq7gfsGZZJdnv1RK6GAXNk_t.jpg"/>
                 </a>
             </div>
         </div>
